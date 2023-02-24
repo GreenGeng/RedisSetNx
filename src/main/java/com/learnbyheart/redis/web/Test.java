1b625b6332c33b7fa1ee5391bb338af1c0761589
gengyuan <2088034881@qq.com>
2023-02-24 20:26:12 +0800

    update

--- src/main/java/com/learnbyheart/redis/web/Test.java
+++ src/main/java/com/learnbyheart/redis/web/Test.java
@@ -24,9 +24,15 @@ public class Test {
     @RequestMapping("/test")
     public String test(){
         String lockKey = "lock:102";
+        // setIf的缺点是不安全 因为这个时间的设置不准 可能会造成误差 （之前的锁可能超时了 还没释放 锁被另一个线程给获取了 就可能会导致死锁）
+        // 设置key 作为锁标记 指定唯一用户标识作为value 确保同一个时间只有一个客户端进程获得锁
+        // 设置过期时间 防止因为系统异常导致key没有被删除 出现死锁
 //        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "gy", 10, TimeUnit.SECONDS);
 //        if(!result)return  "err";
 
+
+        //使用redisson的好处
+        // 里面有一个watchdog机制 可以很好的解决锁续期的问题 能够灵活掌握锁使用的时间
         RLock lock = redissonClient.getLock(lockKey);
         lock.lock();//可重入锁 因为会记录当前的线程的Id
 
@@ -41,6 +47,7 @@ public class Test {
             }
         }finally {
             lock.unlock();
+            //要把key删除 不然一直锁着会死锁
 //            stringRedisTemplate.delete(lockKey);
         }
 
